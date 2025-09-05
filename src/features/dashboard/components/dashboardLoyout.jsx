6fdbd38761f0fd678dffb5addbc63cfd6bf77855
vanessaleivis <vanessaleivis7@gmail.com>
2025-09-04 23:15:21 -0500

    Sexto  cambio

--- src/features/dashboard/components/dashboardLoyout.jsx
+++ src/features/dashboard/components/dashboardLoyout.jsx
@@ -1,5 +1,7 @@
 import { Sidebar } from "./sidebar"
-import { Navbar } from "./navbar2"
+import { Outlet } from "react-router-dom"
+import { Navbar2 } from "./navbar2"
+
 
 
 export function DashboardLayout({ children }) {
@@ -7,8 +9,11 @@ export function DashboardLayout({ children }) {
     <div className="flex h-screen bg-gray-50">
       <Sidebar />
       <div className="flex-1 flex flex-col">
-        <Navbar />
-        <main className="flex-1 p-6 overflow-auto">{children}</main>
+        <Navbar2/>
+        <main className="flex-1 p-6 overflow-auto">
+          <Outlet/>
+        </main>
+        
       </div>
     </div>
   )
